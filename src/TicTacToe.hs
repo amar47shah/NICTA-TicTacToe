e28df2f1a7cf80ee230f786133514b988366be56
Amar Shah <amar47shah@gmail.com>
2016-02-03 22:00:03 -0500

    Add definitions for Board, Cell, and Mark

---
+++ src/TicTacToe.hs
@@ -0,0 +1,27 @@
+module TicTacToe where
+
+import Data.List (intersperse)
+
+data Board = B { aa, ab, ac, ba, bb, bc, ca, cb, cc :: Cell }
+
+instance Show Board where
+  show (B aa ab ac ba bb bc ca cb cc) =
+      unlines
+    . intersperse "--|---|--"
+    . map (concat . intersperse " | " . map show)
+    $ [ [aa, ab, ac]
+      , [ba, bb, bc]
+      , [ca, cb, cc]
+      ]
+
+data Cell = Claimed Mark | Unclaimed
+
+instance Show Cell where
+  show (Claimed m) = show m
+  show Unclaimed   = " "
+
+data Mark = X | O
+
+instance Show Mark where
+  show X = "X"
+  show O = "O"
