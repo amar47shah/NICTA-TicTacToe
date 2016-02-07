2c60836b81c994f6e0c9bdb30b980316c92846e4
Amar Shah <amar47shah@gmail.com>
2016-02-06 22:14:44 -0500

    Use Array for indexed Board

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,23 +1,42 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module TicTacToe where
 
-import Data.List (intersperse)
+import Data.Array
+import Data.List (intercalate, intersperse)
 
-type Game = Either Unfinished Finished
+type Game = Either Finished Unfinished
 
-data Finished = Finished Board
+data Finished = Finished Board Winner
+
+type Winner = Maybe Mark
+
+instance Show Finished where
+  show (Finished b Nothing)  = "DRAW\n" ++ show b
+  show (Finished b (Just m)) = "WINNER: " ++ show m ++ '\n' : show b
+
 data Unfinished = Unfinished Board
 
-data Board = Board { aa, ab, ac, ba, bb, bc, ca, cb, cc :: Cell }
+instance Show Unfinished where
+  show (Unfinished b) = "Unfinished\n" ++ show b
 
-instance Show Board where
-  show (Board aa ab ac ba bb bc ca cb cc) =
-      unlines
-    . intersperse "--|---|--"
-    . map (concat . intersperse " | " . map show)
-    $ [ [aa, ab, ac]
-      , [ba, bb, bc]
-      , [ca, cb, cc]
-      ]
+type Board = Array Position Cell
+
+instance {-# OVERLAPPING #-} Show Board where
+  show = unlines
+       . intersperse bar
+       . (intercalate " | " <$>)
+       . splitEvery dim
+       . (show <$>)
+       . elems
+    where bar = concat $ "-" : replicate (pred dim) "-|--"
+
+type Position = (Coordinate, Coordinate)
+type Coordinate = Int
+
+dim :: Coordinate
+dim = 3
 
 data Cell = Claimed Mark | Unclaimed
 
@@ -25,28 +44,48 @@ instance Show Cell where
   show (Claimed m) = show m
   show Unclaimed   = " "
 
-data Mark = X | O
+data Mark = X | O deriving Show
 
-instance Show Mark where
-  show X = "X"
-  show O = "O"
+move :: Position -> Mark -> Unfinished -> Game
+move p m (Unfinished b)
+   | isFinished $ b' = Left  . Finished b' $ winner b' --inefficient, checks if finished and then again to get winner
+   | otherwise       = Right $ Unfinished b'
+  where b' = move' p m b
 
-type Position = (Coordinate, Coordinate)
+move' :: Position -> Mark -> Board -> Board
+move' p m b =
+  case b ! p of
+    Unclaimed -> b // [(p, Claimed m)]
+    Claimed _ -> b
 
-data Coordinate = A | B | C
+--wrong, just a placeholder
+isFinished :: Board -> Bool
+isFinished = const False
 
--- this is wrong because
--- 1) always returns an unfinished game
--- 2) allows a new move to occupy an already claimed cell
-move :: Mark -> Unfinished -> Position -> Game
-move m (Unfinished b) p =
-  case p of
-    (A, A) -> Left $ Unfinished b { aa = Claimed m }
-    (A, B) -> Left $ Unfinished b { ab = Claimed m }
-    (A, C) -> Left $ Unfinished b { ac = Claimed m }
-    (B, A) -> Left $ Unfinished b { ba = Claimed m }
-    (B, B) -> Left $ Unfinished b { bb = Claimed m }
-    (B, C) -> Left $ Unfinished b { bc = Claimed m }
-    (C, A) -> Left $ Unfinished b { ca = Claimed m }
-    (C, B) -> Left $ Unfinished b { cb = Claimed m }
-    (C, C) -> Left $ Unfinished b { cc = Claimed m }
+--wrong, just a placeholder
+winner :: Board -> Winner
+winner = const $ Just X
+
+start :: Game
+start = Right $ Unfinished empty
+
+empty :: Board
+empty = listArray ((1, 1), (dim, dim)) $ repeat Unclaimed
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n xs = first : splitEvery n rest
+      where
+  (first, rest) = splitAt n xs
+
+sample :: Game
+sample =  start >>=
+  move (1, 1) X >>=
+  move (2, 2) O >>=
+  move (3, 1) X >>=
+  move (2, 1) O >>=
+  move (2, 3) X >>=
+  move (1, 2) O >>=
+  move (3, 2) X >>=
+  move (3, 3) O >>=
+  move (1, 3) X
