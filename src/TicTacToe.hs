bc5953d28aa2c1dac1eb574112ca47ef612e79b6
Amar Shah <amar47shah@gmail.com>
2016-02-07 19:56:36 -0500

    Implement `winner`

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -4,48 +4,46 @@
 
 module TicTacToe where
 
+import Control.Applicative (liftA2)
+import Control.Arrow ((***), (&&&))
+import Control.Monad (join)
 import Data.Array
-import Data.List (intercalate, intersperse)
+import Data.List (find, intercalate, intersperse)
+import Data.Maybe (fromMaybe, isJust, listToMaybe)
 
 type Game = Either Finished Unfinished
-
 data Finished = Finished Board Winner
-
+data Unfinished = Unfinished Board
 type Winner = Maybe Mark
+type Board = Array Position Cell
+type Position = (Coordinate, Coordinate)
+type Coordinate = Int
+type Cell = Maybe Mark
+type Straight = [Cell]
+data Mark = X | O deriving (Eq, Show)
 
 instance Show Finished where
   show (Finished b Nothing)  = "DRAW\n" ++ show b
   show (Finished b (Just m)) = "WINNER: " ++ show m ++ '\n' : show b
 
-data Unfinished = Unfinished Board
-
 instance Show Unfinished where
   show (Unfinished b) = "Unfinished\n" ++ show b
-
-type Board = Array Position Cell
 
 instance {-# OVERLAPPING #-} Show Board where
   show = unlines
        . intersperse bar
        . (intercalate " | " <$>)
        . splitEvery dim
-       . (show <$>)
+       . (draw <$>)
        . elems
     where bar = concat $ "-" : replicate (pred dim) "-|--"
 
-type Position = (Coordinate, Coordinate)
-type Coordinate = Int
+draw :: Cell -> String
+draw (Just m) = show m
+draw _        = " "
 
 dim :: Coordinate
 dim = 3
-
-data Cell = Claimed Mark | Unclaimed
-
-instance Show Cell where
-  show (Claimed m) = show m
-  show _           = " "
-
-data Mark = X | O deriving Show
 
 move :: Position -> Mark -> Unfinished -> Game
 move p m (Unfinished b) = game $ move' p m b
@@ -53,40 +51,52 @@ move p m (Unfinished b) = game $ move' p m b
 move' :: Position -> Mark -> Board -> Board
 move' p m b =
   case b ! p of
-    Unclaimed -> b // [(p, Claimed m)]
-    _         -> b
+    Nothing -> b // [(p, Just m)]
+    _       -> b
 
 game :: Board -> Game
 game b
- | isWon        b = Left . Finished b $ winner b
- | isAllClaimed b = Left $ Finished b Nothing
- | otherwise      = Right $ Unfinished b
+ | isWon  b   = Left . Finished b $ winner b
+ | isFull b   = Left $ Finished b Nothing
+ | otherwise  = Right $ Unfinished b
 
 isWon :: Board -> Bool
-isWon = any sameClaim . straights
+isWon = any same . straights
 
-sameClaim :: [Cell] -> Bool
-sameClaim = undefined
+same :: Straight -> Bool
+same = fromMaybe False
+     . uncurry (liftA2 (||))
+     . ((all (== X) <$>) *** (all (== O) <$>))
+     . (sequence &&& sequence)
 
-straights :: Board -> [[Cell]]
-straights = undefined
+straights :: Board -> [Straight]
+straights b = concatMap ($ b) [rows, columns, diagonals]
 
-isAllClaimed :: Board -> Bool
-isAllClaimed = all isClaimed . elems
+rows :: Board -> [Straight]
+rows b =
+  [ [ b ! (m, n) | n <- [1..dim] ] | m <- [1..dim] ]
 
-isClaimed :: Cell -> Bool
-isClaimed (Claimed _) = True
-isClaimed _           = False
+columns :: Board -> [Straight]
+columns b =
+  [ [ b ! (m, n) | m <- [1..dim] ] | n <- [1..dim] ]
 
---wrong, just a placeholder
+diagonals :: Board -> [Straight]
+diagonals b =
+  [ [ b ! (m, n) | m <- [1..dim], n <- [1..dim], m == n ]
+  , [ b ! (m, n) | m <- [1..dim], n <- [1..dim], m + n == 1 + dim ]
+  ]
+
+isFull :: Board -> Bool
+isFull = all isJust . elems
+
 winner :: Board -> Winner
-winner = const $ Just X
+winner = join . join . (listToMaybe <$>) . find same . straights
 
 start :: Game
 start = Right $ Unfinished empty
 
 empty :: Board
-empty = listArray ((1, 1), (dim, dim)) $ repeat Unclaimed
+empty = listArray ((1, 1), (dim, dim)) $ repeat Nothing
 
 splitEvery :: Int -> [a] -> [[a]]
 splitEvery _ [] = []
@@ -103,5 +113,5 @@ sample =  start >>=
   move (2, 3) X >>=
   move (1, 2) O >>=
   move (3, 2) X >>=
-  move (3, 3) O >>=
-  move (1, 3) X
+  move (1, 3) O >>=
+  move (3, 3) X
