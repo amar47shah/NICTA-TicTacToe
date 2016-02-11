a1a6621bf379dc8362a947b4ec8176fee2e744ca
Amar Shah <amar47shah@gmail.com>
2016-02-10 22:37:11 -0500

    Attempt interactive game shell
    Not trying very hard.

--- src/Main.hs
+++ src/Main.hs
@@ -2,5 +2,14 @@ module Main where
 
 import TicTacToe
 
+import Data.Char (digitToInt)
+
 main :: IO ()
-main = undefined
+main = do
+  let game = start
+  putStrLn $ show game
+  putStr "Your move."
+  position <- (,) <$> (putStr "\nROW: " >> digitToInt <$> getChar) <*> (putStr "\nCOLUMN: " >> digitToInt <$> getChar)
+  putStrLn ""
+  let game' = game >>= move position
+  putStrLn $ show game'
