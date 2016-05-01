9253015d14b656cb06671b65231fa7e4a2c74ee7
Amar Shah <amar47shah@gmail.com>
2016-05-01 01:15:03 -0400

    Add randomMove and randomTurn

--- src/Opponent.hs
+++ src/Opponent.hs
@@ -4,6 +4,12 @@ import Game.Internal
 
 import System.Random (getStdRandom, randomR)
 
+randomTurn :: Game -> IO Game
+randomTurn g = (g >>=) <$> randomMove
+
+randomMove :: IO (Unfinished -> Game)
+randomMove = move <$> randomPosition
+
 randomPosition :: IO Position
 randomPosition = (,) <$> randomCoordinate <*> randomCoordinate
 
